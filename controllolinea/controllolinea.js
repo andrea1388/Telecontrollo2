d0c4fa8e7e5399e08e6ccb00c938cf047a79d1da
Andrea Carrara <andrea.carrara@libero.it>
2018-12-22 15:47:02 +0100

    Update controllolinea.js

--- controllolinea/controllolinea.js
+++ controllolinea/controllolinea.js
@@ -5,12 +5,32 @@
 
     22/12/2018 Andrea Carrara iw3gcb
 */
+const i2c = require('i2c-bus');
+const PCF1 = 0x48;
+const PCF2 = 0x49;
 
 var numparametri = process.argv.length;
+setlinea(1,1);
 
+/*
 if(numparametri>=4) leggilinee();
 else {
     var cmd=process.argv[2].toLowerCase();
     if(cmd=="on")
 }
+*/
 
+function setlinea(linea, valore) {
+    var add=PCF1;
+    var bit=linea-1;
+    il (linea>8) 
+    {
+        add=PCF2;
+        bit=linea-8;
+    }
+    var val=i2c1.receiveByteSync(add);
+    if(valore==true)
+        i2c1.sendByteSync(add, val | Math.pow(2,bit));
+    else
+        i2c1.sendByteSync(add, val & (255-Math.pow(2,bit));
+}
