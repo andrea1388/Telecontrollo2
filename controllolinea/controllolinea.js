db97916799a6b572a48c53bdf077e8d508679772
Andrea rasp <andrea.carrara@libero.it>
2018-12-22 16:59:00 +0100

    onoff ok

--- controllolinea/controllolinea.js
+++ controllolinea/controllolinea.js
@@ -6,11 +6,11 @@
     22/12/2018 Andrea Carrara iw3gcb
 */
 const i2c = require('i2c-bus');
-const PCF1 = 0x48;
-const PCF2 = 0x49;
+const PCF1 = 0x38;
+const PCF2 = 0x39;
 
 var numparametri = process.argv.length;
-setlinea(1,1);
+setlinea(1,0);
 
 /*
 if(numparametri>=4) leggilinee();
@@ -21,16 +21,19 @@ else {
 */
 
 function setlinea(linea, valore) {
+    const i2c1 = i2c.openSync(1);
     var add=PCF1;
     var bit=linea-1;
-    il (linea>8) 
+    if(linea>8)
     {
         add=PCF2;
         bit=linea-8;
     }
     var val=i2c1.receiveByteSync(add);
+    console.log(add,val,bit);
     if(valore==true)
         i2c1.sendByteSync(add, val | Math.pow(2,bit));
     else
-        i2c1.sendByteSync(add, val & (255-Math.pow(2,bit));
+        i2c1.sendByteSync(add, val & (255-Math.pow(2,bit)));
+    i2c1.closeSync();
 }
