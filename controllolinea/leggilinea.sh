b2e8d9d3d4a0ec88372536545b97dfd226ae1e21
Andrea tlc buono <andrea.carrara@libero.it>
2019-07-23 17:40:04 +0000

    leggilinea ok

--- controllolinea/leggilinea.sh
+++ controllolinea/leggilinea.sh
@@ -9,10 +9,17 @@ then
     exit
 fi
 l=$1 # il numero della linea da leggere
+b=$((2**$(($l-1))))
 
 #legge lo stato attuale
 cd /usr/Telecontrollo2/controllolinea
 sl1=$(./leggilinee.sh)
 
-out=$((sl1 & $l))
+out=$((sl1 & $b))
+
+if [ $out -gt 1 ]
+then
+    out=1
+fi
+
 echo $out
